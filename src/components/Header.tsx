33c8946ba38b54a023097b2669576913b6c3a386
Moa Hedendahl <moahedendahl@gmail.com>
2023-08-10 10:31:20 +0200

    add a header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,157 @@
-import { AppBar } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import React from 'react';
+
+const pages = ['Products', 'Pricing', 'Blog'];
 
 function Header() {
-  return <AppBar></AppBar>;
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar
+      position="static"
+      elevation={0}
+      sx={{ width: '100%', backgroundColor: '#fff' }}
+    >
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          backgroundColor: '#fff',
+          padding: '0px !important',
+          margin: '0px',
+          width: '100%',
+        }}
+      >
+        <Toolbar
+          disableGutters
+          sx={{
+            justifyContent: {
+              xs: 'space-between',
+              md: 'center',
+            },
+            backgroundColor: '#fff',
+          }}
+        >
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              display: { xs: 'none', md: 'flex' },
+              justifyContent: 'center',
+              fontFamily: 'gelica-black',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: '#141414',
+              textDecoration: 'none',
+            }}
+          >
+            Sun pop
+          </Typography>
+
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'gelica-black',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Sun pop
+          </Typography>
+
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: { xs: 'none', md: 'flex' },
+            justifyContent: 'center',
+            background: '#FDE9EA',
+          }}
+        >
+          {pages.map((page) => (
+            <Button
+              key={page}
+              onClick={handleCloseNavMenu}
+              sx={{
+                my: 2,
+                color: '#141414',
+                display: 'block',
+                fontFamily: 'gelica-regular',
+              }}
+            >
+              {page}
+            </Button>
+          ))}
+        </Box>
+      </Container>
+    </AppBar>
+  );
 }
 
 export default Header;

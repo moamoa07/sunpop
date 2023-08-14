1133ba6e8f6468792b6806b4dadb2c4312ef35c5
Moa Hedendahl <moahedendahl@gmail.com>
2023-08-14 19:30:22 +0200

    changed styling

--- src/components/Product1.tsx
+++ src/components/Product1.tsx
@@ -2,49 +2,85 @@ import { Box, Button, Typography } from '@mui/material';
 
 function Product1() {
   return (
-    <Box sx={{ backgroundColor: '#E9FDED', height: '100vh', display: 'flex' }}>
-      <Box sx={{ display: 'flex', flex: 1 }}>
-        <img
-          src="./src/assets/images/PLsoda.png"
-          alt=""
-          style={{ width: '100%', height: '100%' }}
-        />
-      </Box>
-      <Box
-        sx={{
-          display: 'flex',
-          flex: 1,
-          flexDirection: 'column',
-          marginTop: '10%',
-        }}
-      >
-        <img
-          src="./src/assets/images/Peach&Lime.png"
-          alt=""
-          style={{ width: '30%' }}
-        />
-        <Typography
-          sx={{ width: '80%', marginTop: '1rem', fontFamily: 'arial' }}
-        >
-          Quench your thirst and indulge in the vibrant fusion of natural peach
-          and zesty lime with every sip of our revitalizing soda. Crafted with
-          care, this effervescent beverage not only tantalizes your taste buds
-          but also elevates your senses, leaving you feeling rejuvenated and
-          invigorated.
-        </Typography>
-        <Button
-          variant="contained"
-          disableElevation={true}
+    <Box
+      sx={{
+        backgroundColor: '#E9FDED',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        maxHeight: '800px',
+      }}
+    >
+      <Box sx={{ display: 'flex', flex: 1, position: 'relative' }}>
+        <Box
           sx={{
-            backgroundColor: '#F58A62',
-            borderRadius: '50rem',
-            width: '25%',
-            fontFamily: 'Gelica-regular',
-            marginTop: '1rem',
+            position: 'absolute',
+            right: '9rem',
+            top: '2rem',
+            zIndex: '1',
           }}
         >
-          Buy now
-        </Button>
+          <img src="./src/assets/images/lemon.png" alt="" />
+        </Box>
+        <Box sx={{ zIndex: '2' }}>
+          <img
+            src="./src/assets/images/PLsoda.png"
+            alt=""
+            style={{ width: '100%' }}
+          />
+        </Box>
+      </Box>
+      <Box sx={{ display: 'flex', flex: 1, position: 'relative' }}>
+        <Box
+          sx={{
+            flexDirection: 'column',
+            // marginTop: '10%',
+          }}
+        >
+          <img
+            src="./src/assets/images/Peach&Lime.png"
+            alt=""
+            style={{ width: '30%' }}
+          />
+          <Typography
+            sx={{
+              width: '80%',
+              marginTop: '1rem',
+              fontFamily: 'arial',
+              letterSpacing: 4,
+            }}
+          >
+            Quench your thirst and indulge in the vibrant fusion of natural
+            peach and zesty lime with every sip of our revitalizing soda.
+            Crafted with care, this effervescent beverage not only tantalizes
+            your taste buds but also elevates your senses, leaving you feeling
+            rejuvenated and invigorated.
+          </Typography>
+          <Button
+            variant="contained"
+            disableElevation={true}
+            sx={{
+              backgroundColor: '#F58A62',
+              borderRadius: '50rem',
+              width: '25%',
+              fontFamily: 'Gelica-regular',
+              marginTop: '3rem',
+            }}
+          >
+            Buy now
+          </Button>
+          <Box
+            sx={{
+              position: 'absolute',
+              right: '9rem',
+              bottom: '-5rem',
+              zIndex: '1',
+              rotate: '90deg',
+            }}
+          >
+            <img src="./src/assets/images/peach.png" alt="" />
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
